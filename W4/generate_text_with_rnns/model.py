37e1e09a34fd9e98c633cb6083ad1f43a36f7d04
Mark Barbaric <markantonybarbaric@hotmail.com>
2023-11-19 13:46:31 +0000

    working on tensorflow tutorial on text_generation.

---
+++ W4/generate_text_with_rnns/model.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tf.keras  import Model
+
+class RNNModel(Model):
+    def __init__(self, vocab_size, embedding_dim, rnn_units):
+        super().__init__(self)
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(rnn_units,
+                                       return_sequences=True,
+                                       return_state=True)
+        self.dense = tf.keras.layers.Dense(vocab_size)
+
+    def call(self, inputs, states=None, return_state=False, training=False):
+        x = inputs
+        s = self.embedding(x, training)
+
+        if states is None:
+            states = self.gru.get_initial_state(x)
+        
+        x, states = self.gru(x, initial_state=states, training=training)
+        x = self.Dense(x, training=training)
+
+        if return_state:
+            return x, states
+        else:
+            return x
+            
